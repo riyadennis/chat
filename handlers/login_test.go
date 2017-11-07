674cbe5d5597bb87d7a16c8e3a403e6ddab1043b
Riya Dennis <riyacv@gmail.com>
2017-11-07 21:23:04 +0000

    moved test for login

---
+++ handlers/login_test.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"net/http"
+)
+
+func TestNewLoginProviderHandler(t *testing.T) {
+	lph := NewLoginHandler()
+	assert.IsType(t, &loginHandler{}, lph)
+}
+func TestLoginHandlerServeHTTPWillGiveOKForValidLoginURL(t *testing.T) {
+	req, err := http.NewRequest("GET", "/auth/login/google", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	lh := NewLoginHandler()
+	handler := MustAuth(http.Handler(lh))
+	handler.ServeHTTP(rr, req)
+	assert.Equal(t, rr.Code, http.StatusTemporaryRedirect)
+}
+func TestLoginHandlerServeHTTPWillGiveErrorForInvalidValidLoginURL(t *testing.T) {
+	req, err := http.NewRequest("GET", "/auth/userlogin/facebook", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	lph := NewLoginHandler()
+
+	handler := MustAuth(http.Handler(lph))
+	handler.ServeHTTP(rr, req)
+	assert.Equal(t, rr.Code, http.StatusTemporaryRedirect)
+}
+
